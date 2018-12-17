ac84c47556cda770abd7b75df72ea4fd38df0099
BohdanProkaza <45563904+BohdanProkaza@users.noreply.github.com>
2018-12-17 09:37:49 +0200

    Update пр7.cpp

--- пр7.cpp
+++ пр7.cpp
@@ -1,28 +1,57 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
- 
-int main (){
-	int n, amax = -1, amin = 1000000, sum, kmax = 0, kmin = 0;
-	int a[10]= {0};
-	
-	cin >> n;
-	for(int i=1;i<=n;i++){
-		cin >> a[i];
-		sum += a[i];
-		if(a[i]<amin){
-			amin = a[i];
-			//kmin++;
-		}
-		if(a[i]>amax){
-		amax = a[i];
-		//kmax++;
-		}
+
+void in_array(int arr[], int narr){
+	srand(time(NULL));
+	for(int i=0;i<narr;i++){
+		arr[i] = rand();
 	}
-	sum = sum - amax - amin;
-	cout << 1.0 * sum / (n-2) << endl;
+}
+
+int find_min(int arr[], int narr, int min_arr){
+	for(int i=0;i<narr;i++){
+		if(arr[i] < min_arr){
+			min_arr = arr[i];
+		}		
+	}
+	return min_arr;
+}
+
+int find_max(int arr[], int narr, int max_arr){
+	for(int i=0;i<narr;i++){
+		if(arr[i]>max_arr){
+			max_arr = arr[i];
+		}		
+	}
+	return max_arr;
+}
+
+int sum_arr(int arr[], int narr, int max_arr, int min_arr, int s_arr){
+	for(int i=0;i<narr;i++){
+		if(arr[i] != max_arr && arr[i] != min_arr){
+			s_arr += arr[i];
+		}		
+	}
+	return s_arr;
+}
+
+
+void output_arr(int sum_arr, int narr){
+	cout << sum_arr * 1.0 / (narr - 2) << endl;
+}
+
 	
-	//sum = sum - kmin * amax - kmax * amin;
-	//cout << 1.0 * sum / (n-kmin-kmax) << endl;
+int main (){
+	int n, amax = -1, amin = 100000000, sum = 0, kmax = 0, kmin = 0;
+	int a[100] = {0};
 	
-  return 0;
+	cin >> n;	
+	
+	in_array(a,n);	
+	amin = find_min(a,n,amin);			cout << amin << endl;
+	amax = find_max(a,n,amax);			cout << amax << endl;
+	sum = sum_arr(a,n,amax, amin, sum);	cout << sum << endl;
+	output_arr(sum,n);	
+
+	return 0;
 }
